0f295221e13e23fbbaee9412832263f7782cab42
Gleb Klyga <klyga90@bk.ru>
2024-07-25 14:23:14 +0300

    Initial Commit

---
+++ ReduxProject/ReduxProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxProjectApp.swift
+//  ReduxProject
+//
+//  Created by Глеб Клыга on 25.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ReduxProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
